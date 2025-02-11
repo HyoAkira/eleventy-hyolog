45c00925ceff7da4eb869c31828a5c28aa4f09a9
hyo <hyosuke@yandex.com>
2025-02-12 00:46:57 +0300

    a

--- .eleventy.config.js
+++ .eleventy.config.js
@@ -1,7 +1,10 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
     return {
-        pathPrefix: "eleventy-hyolog",
+        pathPrefix: "/eleventy-hyolog/",
 
     };
 };
