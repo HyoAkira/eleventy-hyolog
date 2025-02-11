6f74a2e3c860fa139161abb246d6f4dce3a565b6
hyo <hyosuke@yandex.com>
2025-02-11 23:22:14 +0300

    test

--- _data/flowers.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-export default async function () {
-    try {
-        const response = await axios.get('https://flowers-mock-data.firebaseio.com/flowers.json');
-        return response.data;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
