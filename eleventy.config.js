4538037505c66d35616ec136abab220b2a03bbe4
hyo <hyosuke@yandex.com>
2025-02-12 01:16:14 +0300

    ccuuu

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,9 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy"
 export default function (eleventyConfig) {
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-    eleventyConfig.addFilter("gurl", function (url) {
-        return `/eleventy-hyolog${url}`;
-    });
+    // eleventyConfig.addFilter("gurl", function (url) {
+    //     return `/eleventy-hyolog${url}`;
+    // });
     return {
         pathPrefix: "/eleventy-hyolog/",
 
