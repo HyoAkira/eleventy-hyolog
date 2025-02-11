99b30446a4feb2f38a280004ea1bc35d2cc8c88e
hyo <hyosuke@yandex.com>
2025-02-12 01:09:08 +0300

    aaa

--- .eleventy.config.js
+++
@@ -1,9 +0,0 @@
-import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
-
-export default function (eleventyConfig) {
-    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-};
-
-export const config = {
-    pathPrefix: "/eleventy-hyolog/",
-}
